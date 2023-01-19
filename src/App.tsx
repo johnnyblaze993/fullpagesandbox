28019e3b0e33c8ee552a56094cd6f987d5c20c90
johnnyblaze993 <107284554+johnnyblaze993@users.noreply.github.com>
2023-01-19 14:06:52 -0500

    eh, not the scroll effect i wanted

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,44 @@
-import React from "react";
+import { Element, scroller } from "react-scroll";
+import { Link } from "react-scroll";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <h1>React + TypeScript + Webpack</h1>
+    <div>
+      <Link to="section1" smooth={true} duration={500}>
+        Go to Section 1
+      </Link>
+      <Link to="section2" smooth={true} duration={500}>
+        Go to Section 2
+      </Link>
+      <Link to="section3" smooth={true} duration={500}>
+        Go to Section 3
+      </Link>
+
+      <Element name="section1" style={{ height: "100vh" }}>
+        <h1>Section 1</h1>
+      </Element>
+      <Element name="section2" style={{ height: "100vh" }}>
+        <h1>Section 2</h1>
+        <Link to="subsection1" smooth={true} duration={500}>
+          Go to Subsection 2.1
+        </Link>
+        <Link to="subsection2" smooth={true} duration={500}>
+          Go to Subsection 2.2
+        </Link>
+        <Element name="subsection1" style={{ height: "100vh" }}>
+          <h2>Subsection 2.1</h2>
+        </Element>
+        <Element name="subsection2" style={{ height: "100vh" }}>
+          <h2>Subsection 2.2</h2>
+        </Element>
+      </Element>
+      <Element name="section3" style={{ height: "100vh" }}>
+        <h1>Section 3</h1>
+      </Element>
     </div>
   );
-}
+};
 
 export default App;
+
+//
